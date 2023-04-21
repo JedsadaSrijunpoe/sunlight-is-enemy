ad4d80ad2d5c8c609c9b93558b96c6ed4a67513b
sirapopModel <model12500@gmail.com>
2023-04-21 16:11:02 +0700

    Fixed collision lv1

--- World/switch.gd
+++ World/switch.gd
@@ -2,12 +2,17 @@ extends Area2D
 
 # entered will be true when player enter switch area.
 var entered = false
-
 var activated = false
 signal switch_state_changed(activated)
 
 func _process(_delta):
 	# Toggle the switch
+	
+	if not entered:
+		$Label.visible = false
+	else:
+		$Label.visible = true
+		
 	if entered and Input.is_action_just_pressed("interact"):
 		activated = not activated
 		switch_state_changed.emit(activated)
