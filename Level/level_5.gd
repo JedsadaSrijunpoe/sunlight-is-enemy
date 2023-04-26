f715e1e211a5a074f6c80b181b12e49028ba6d98
SupakornPholsiri <supakorncpw@gmail.com>
2023-04-26 18:11:04 +0700

    Level 5 connected to ending cutscene

--- Level/level_5.gd
+++ Level/level_5.gd
@@ -4,27 +4,34 @@ extends Node2D
 @onready var animation_player = $AnimationPlayer
 @onready var vampire_sword = $VampireSword
 @onready var vampire_sword_2 = $VampireSword2
+@onready var circular_time = $CircularTime
+@onready var label = $Label
 
 var sword_amount
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	label.hide()
 	var temp_sword_amount = 0
 	for sword in swordgroup.get_children():
 		temp_sword_amount += 1
 		sword.connect("sword_picked_up", count_sword)
 	sword_amount = temp_sword_amount
-	print(sword_amount)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("interact") and sword_amount == 0:
+		final_cutscene()
 	
 func count_sword():
 	sword_amount -= 1
-	print(sword_amount)
 	if sword_amount == 0:
-		reveal_switch()
+		circular_time.get_node("AnimationPlayer").disconnect("animation_finished", circular_time._on_animation_player_animation_finished)
+		circular_time.get_node("Timer").stop()
+		if circular_time.IsDay:
+			circular_time.get_node("AnimationPlayer").play("DayChange")
+		label.show()
 	
-func reveal_switch():
+func final_cutscene():
+	label.hide()
 	animation_player.play("Final_cutscene")
