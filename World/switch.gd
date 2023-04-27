bca0640d93c68009188f3827bab8e747da03e7d0
SupakornPholsiri <supakorncpw@gmail.com>
2023-04-28 00:30:31 +0700

    Improved switch

--- World/switch.gd
+++ World/switch.gd
@@ -1,6 +1,9 @@
 extends Area2D
 
+@onready var collision_shape = $CollisionShape2D
+
 # entered will be true when player enter switch area.
+@export var ONE_SHOT : bool = true
 var entered = false
 var activated = false
 signal switch_state_changed(activated)
@@ -20,7 +23,11 @@ func _process(_delta):
 	
 	# Flip the switch sprite
 	if activated:
-		$Sprite2D.flip_h = true
+		if ONE_SHOT :
+			$Sprite2D.frame = 2
+			collision_shape.disabled = true
+		else:
+			$Sprite2D.frame = 1
 	else:
-		$Sprite2D.flip_h = false
+		$Sprite2D.frame = 0
 
